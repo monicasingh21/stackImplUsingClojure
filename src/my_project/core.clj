8f9b2c6c72af664a77d5a8d98e7551393cbe37cb
Monica Singh <27210@telxsids.net>
2021-11-02 13:48:57 +0530

    Added inbuilt functions

--- src/my_project/core.clj
+++ src/my_project/core.clj
@@ -17,42 +17,20 @@
     (println "Stack Overflow!!!")
     (do
       (swap! top inc)
-      (def input (list* val input))
+      (def input (conj input val))
       (println "Pushing the element into stack, updated Stack is:" input)
-      (mc/insert-and-return db coll {:_id (ObjectId.) :operation "PUSH" :updatedList input})
-      )      
-    ))
+      (mc/insert-and-return db coll {:_id (ObjectId.) :operation "PUSH" :updatedList input}))))
 
-
-(defn popst
-  []
-  (if (< @top 0)
-    (println "Stack Empty!!!")
-    (do
-      (swap! top dec)
-      (def input (rest input))
-      (println "Element deleted, updated Stack is:" input)
-      (mc/insert-and-return db coll {:operation "POP" :updatedList input})
-      )
-      
-    ))
-
-(defn peekst []
-  (if (< @top 0)
-    (println "Stack Empty!!!")
-    (println "Top element in Stack :" (nth input 0))))
 
 (defn show_all_operations []
   (def stack_data (mc/find-maps db coll))
   (loop [x 0]
     (when (< x (count stack_data))
-       (let [sdata (nth stack_data x)]
-         (def operation ((select-keys sdata [:operation]) :operation))
-         (def updatedList ((select-keys sdata [:updatedList]) :updatedList))
-       )
+      (let [sdata (nth stack_data x)]
+        (def operation ((select-keys sdata [:operation]) :operation))
+        (def updatedList ((select-keys sdata [:updatedList]) :updatedList)))
       (println "Operation:  " operation "\t Updated List:  " updatedList)
-      (recur (+ x 1))))
-  )
+      (recur (+ x 1)))))
 
 (defn -main
   [& args]
@@ -61,35 +39,57 @@
 
   (def con 0)
   (println "Welcome to Stack World!!!")
-  (while (not= con 5)
+  (while (not= con 7)
     (do
       (println "Please select your input : ")
       (println " 1. Push element into stack")
       (println " 2. Pop element from stack")
       (println " 3. Peek element from stack")
       (println " 4. Display elements of stack")
-      (println " 5. Exit")
+      (println " 5. Delete an element from a particular index")
+      (println " 6. Insert an element at a particular index")
+      (println " 7. Exit")
       (def con (Integer. (re-find #"\d+" (read-line))))
       (cond
         (= con 1)
         (do
           (println "Please enter your value to insert into stack : ")
           (let [value (Integer. (re-find #"\d+" (read-line)))] (push value)))
-        (= con 2) (popst)
-        (= con 3) (peekst)
+        (= con 2) (try
+                    (def input (pop input))
+                    (println "Element deleted, updated Stack is: " input)
+                    (mc/insert-and-return db coll {:operation "POP" :updatedList input})
+                    (catch Exception e (println (str "caught exception: " (.toString e)))))
+        (= con 3) (println "Top element in the stack: " (peek input))
         (= con 4)
         (do
           (mc/find-maps db "stack")
           (println "Elements in stack: " input))
-        (= con 5) (println "Thanks!!!")
+        (= con 5) (if (> (count input) 0)
+                    (do
+                      (println "Please enter the position you want to delete: ")
+                      (let [pos (Integer. (re-find #"\d+" (read-line)))]
+                        (def input (apply list (remove #{(nth input pos)} input)))
+                        (println "Element deleted, updated list: " input)
+                        (mc/insert-and-return db coll {:_id (ObjectId.) :operation "DLTATPOST" :updatedList input})))
+                    (println "Stack is empty !!!!"))
+        (= con 6) (if (< (count input) MAX)
+                    (do
+                      (println "Please enter the position and value you want to insert: ")
+                      (let [pos (Integer. (re-find #"\d+" (read-line)))
+                            value (Integer. (re-find #"\d+" (read-line)))]
+                        (def input (apply list (concat (concat (take pos input) (list value)) (take-last (- (count input) pos) input))))
+                        (println "Element inserted, updated list: " input)
+                        (mc/insert-and-return db coll {:_id (ObjectId.) :operation "INSRTATPOST" :updatedList input})))
+                    (println "Stack overflow !!!!"))
+        (= con 7) (println "Thanks!!!")
         :else (println "Invalid Option, please retry..."))))
-        
 
-        (println "Number of PUSH operations done" (mc/count db coll {:operation "PUSH"}))
-        (println "Number of POP operations done" (mc/count db coll {:operation "POP"}))
-        
-        (println "--------------------------------------")
-        (show_all_operations)
-        (println "--------------------------------------")
-        (println)
-        )
+
+  (println "Number of PUSH operations done" (mc/count db coll {:operation "PUSH"}))
+  (println "Number of POP operations done" (mc/count db coll {:operation "POP"}))
+
+  (println "--------------------------------------")
+  (show_all_operations)
+  (println "--------------------------------------")
+  (println))
